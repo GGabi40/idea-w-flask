2346ba1647d1304957cb55d5cbcaacd4c95b1c3f
ggabi40 <gabibc2000@hotmail.com>
2024-10-29 22:58:15 -0300

    error 404 and 500 added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask import render_template
 app = create_app()
 
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -17,6 +18,17 @@ def database():
     init_db()
     return "Base de datos creada correctamente."
 
+
+""" Método para manejar errores 404 """
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('errors/error404.html', error=error)
+
+
+""" Método para manejar errores 500 """
+@app.errorhandler(500)
+def internal_server_error():
+    return render_template('errors/error500.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
